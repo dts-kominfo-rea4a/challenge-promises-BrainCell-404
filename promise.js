a59aa5ad88f8ecbe060d1fdeb0a8ebd83e7b2b83
BrainCell-404 <braincellno404@gmail.com>
2022-09-17 09:33:15 +0700

    Tugas 2 : Menyelesaikan Challenge Promise

--- promise.js
+++ promise.js
@@ -1,7 +1,26 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+// const promiseOutput = null
+const promiseOutput = async (emosi) => {
+  // menggabungkan semua data dari bioskop
+  const ixx = await promiseTheaterIXX();
+  const vcg = await promiseTheaterVGC();
+  const gabungData = [...ixx, ...vcg];
+
+  // variabel utuk hasil akhir
+  let hitungEmosi = 0;
+
+  // looping untuk memfilter berdasarkan emosi yang dicari
+  gabungData.forEach((perOrang) => {
+    if (perOrang.hasil === emosi) {
+      hitungEmosi++;
+    }
+  });
+
+  // mengembalikan nilai dari hasil akhir yang sudah di filtering
+  return hitungEmosi;
+};
 
 module.exports = {
   promiseOutput,
